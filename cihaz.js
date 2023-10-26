6735b23d8223b36038ebe2c9375f90aeabeaa8de
parzivalhaliday <ali_dinc@outlook.com.tr>
2023-10-26 15:57:51 +0300

    bi şeyler ekledim

--- cihaz.js
+++ cihaz.js
@@ -1,16 +1,31 @@
 window.addEventListener("deviceorientation", handleOrientation, true);
 
 function handleOrientation(event) {
-    var absolute = event.absolute;
-    var alpha    = event.alpha; // Yön açısı (0-360 derece)
-    var beta     = event.beta;  // Yatay eğiklik açısı (-180-180 derece)
-    var gamma    = event.gamma; // Dikey eğiklik açısı (-90-90 derece)
+    var beta = event.beta;  // Yatay eğiklik açısı (-180-180 derece)
 
     if (beta > 20) {
-        console.log("Cihaz sağa eğik.");
+        showAlert("Cihaz sağa eğik.");
     } else if (beta < -20) {
-        console.log("Cihaz sola eğik.");
+        showAlert("Cihaz sola eğik.");
     } else {
-        console.log("Cihaz düz.");
+        showAlert("Cihaz düz.");
     }
+}
+
+function showAlert(message) {
+    var alertBox = document.createElement("div");
+    alertBox.style.position = "fixed";
+    alertBox.style.top = "10px";
+    alertBox.style.left = "10px";
+    alertBox.style.background = "rgba(255, 0, 0, 0.8)";
+    alertBox.style.color = "white";
+    alertBox.style.padding = "10px";
+    alertBox.style.borderRadius = "5px";
+    alertBox.textContent = message;
+
+    document.body.appendChild(alertBox);
+
+    setTimeout(function() {
+        document.body.removeChild(alertBox);
+    }, 3000); // Uyarı 3 saniye sonra kaybolur
 }
