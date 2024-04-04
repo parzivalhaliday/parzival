9be8f0ee0f8879cfc06f83715e46ce3550381a19
parzivalhaliday <ali_dinc@outlook.com.tr>
2024-04-04 19:19:45 +0300

    bi şeyler ekledim

---
+++ colorboxes.js
@@ -0,0 +1,97 @@
+new p5();
+
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
+}
+
+let grid;
+let cols;
+let rows;
+let resolution = 20; // Sabit bir değer kullanabilirsiniz
+let cnv;
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    cols = floor(width / resolution);
+    rows = floor(height / resolution);
+    grid = make2DArray(cols, rows);
+
+    // Yeni pencere boyutuna göre grid'i yeniden başlat
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
+    }
+}
+
+function setup() {
+    frameRate(10);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    cols = floor(width / resolution);
+    rows = floor(height / resolution);
+    grid = make2DArray(cols, rows);
+
+    // İlk başlangıçta rastgele grid oluştur
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
+    }
+}
+
+function draw() {
+    background("#23241f");
+
+    // Grid'i çiz
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(random(255), random(255), random(255));
+                stroke(100);
+                rect(x, y, resolution - 1, resolution - 1);
+            }
+        }
+    }
+
+    let next = make2DArray(cols, rows);
+
+    // Compute next based on grid
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            let neighbors = countNeighbors(grid, i, j);
+
+            // Kurallara göre grid'i güncelle
+            if (state == 0 && neighbors == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
+            }
+        }
+    }
+
+    grid = next;
+}
+
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
+}
