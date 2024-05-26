ac7b545d3fc5e9899d0e2ed1579e7e3d0914cbd2
parzivalhaliday <ali_dinc@outlook.com.tr>
2024-05-26 04:15:51 +0300

    bi şeyler ekledim

---
+++ movie.js
@@ -0,0 +1,20 @@
+fetch('https://api.themoviedb.org/3/list/8301529?api_key=0f07e31867110e665a9b2c304b153eb9')
+  .then(response => response.json())
+  .then(data => {
+    // IMDb puanına göre sırala
+    data.items.sort((a, b) => b.vote_average - a.vote_average);
+    // Her bir film öğesini döngüyle geç
+    const movieList = document.getElementById('movieList');
+    data.items.forEach(movie => {
+      const listItem = document.createElement('li');
+      // Başlık boşsa ismi kullan
+      const title = movie.title ? movie.title : movie.name;
+      // Arka plan görseli
+      const backdropPath = movie.backdrop_path ? 'https://image.tmdb.org/t/p/w500' + movie.backdrop_path : '';
+      listItem.innerHTML = `<div>${title}</div>`;
+      movieList.appendChild(listItem);
+    });
+  })
+  .catch(error => {
+    console.error('API\'den veri alınamadı:', error);
+  });
