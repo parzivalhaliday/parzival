24008ac40d30828dfa7ad33481332e79410fdbe6
parzivalhaliday <ali_dinc@outlook.com.tr>
2025-06-28 15:54:53 +0300

    bi şeyler ekledim

--- js/ip.js
+++ js/ip.js
@@ -6,7 +6,7 @@ fetch('https://api.ipify.org?format=json')
     if (userIP === '88.241.86.109') {
         // Özel sayfaya yönlendir
         window.location.href = 'ipozel.html';
-    } else if (userIP === '78.190.56.107') {
+    } else if (userIP === '95.70.171.32') {
         // İkinci özel IP için yönlendirme
         window.location.href = 'ipozel2.html';
     }
